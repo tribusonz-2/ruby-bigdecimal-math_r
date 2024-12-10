4c18f5ab8a499e6dd5d743774ffd104bf2f93f57
Hironobu Inatsuka <168434667+tribusonz-2@users.noreply.github.com>
2024-12-11 08:42:59 +0900

    Fixed to give more accurate values
    We have modified it to give a more accurate value. 
    
    The lack of precision of about 17 digits in $\log\Gamma(\frac{1}{2})$ should be improved.
    The implementation is currently a routine that separates digits with the #round method.
    This may be something that should be removed.
    
    > BigMath.lgamma(1/2r, 32)
    # => 0.57236494292470006131023065442898e0
    #    0.57236494292470008707171367567652935582364740645765578575681153573606888... in Wolfram|Alpha

--- bigmath.rb
+++ bigmath.rb
@@ -8,6 +8,8 @@ module BigMath
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
 
+    x = x.to_d(prec) unless x.class == BigDecimal
+
     return -BigDecimal::INFINITY if x.zero?
     if x.positive?
       return BigDecimal::INFINITY if x.infinite?
@@ -20,6 +22,8 @@ module BigMath
   def log10(x, prec)
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
+
+    x = x.to_d(prec) unless x.class == BigDecimal
 
     return -BigDecimal::INFINITY if x.zero?
     if x.positive?
@@ -34,6 +38,8 @@ module BigMath
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
 
+    x = x.to_d(prec) unless x.class == BigDecimal
+
     return x if x.nan? || x.infinite?
     sign = x.negative? ? -1 : 1
     y = sign * BigDecimal((sign * x).to_s, prec) ** Rational(1, 3).to_d(prec)
@@ -43,6 +49,8 @@ module BigMath
   def exp2(x, prec)
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
+
+    x = x.to_d(prec) unless x.class == BigDecimal
 
     return x if x.nan?
     x *= log(BigDecimal('2'), prec) 
@@ -54,6 +62,8 @@ module BigMath
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
 
+    x = x.to_d(prec) unless x.class == BigDecimal
+
     return x if x.nan? || x.infinite?
     y = sin(x, prec) / cos(x, prec)
     y.round(prec)
@@ -62,6 +72,8 @@ module BigMath
   def asin(x, prec)
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
+
+    x = x.to_d(prec) unless x.class == BigDecimal
 
     return x if x.nan?
     if (x >= -1 && x <= 1)
@@ -75,9 +87,18 @@ module BigMath
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
 
+    x = x.to_d(prec) unless x.class == BigDecimal
+
     return x if x.nan?
     if (x >= -1 && x <= 1)
-      y = PI(prec) / 2 - asin(x, prec)
+      y = 0
+      if x == 1
+        y = BigDecimal('0', prec)
+      elsif x == -1
+        y = PI(prec)
+      else
+        y = PI(prec) / 2 - asin(x, prec)
+      end
       return y.round(prec)
     end
     BigDecimal::NAN
@@ -86,6 +107,8 @@ module BigMath
   def sinh(x, prec)
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
+
+    x = x.to_d(prec) unless x.class == BigDecimal
 
     return x if (x.nan? || x.infinite?)
     exppx = exp( x, prec)
@@ -98,6 +121,8 @@ module BigMath
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
 
+    x = x.to_d(prec) unless x.class == BigDecimal
+
     return x if x.nan?
     return BigDecimal::INFINITY if x.infinite?
     exppx = exp( x, prec)
@@ -109,6 +134,8 @@ module BigMath
   def tanh(x, prec)
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
+
+    x = x.to_d(prec) unless x.class == BigDecimal
 
     return x if x.nan?
     if x.infinite?
@@ -124,6 +151,8 @@ module BigMath
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
 
+    x = x.to_d(prec) unless x.class == BigDecimal
+
     return x if (x.nan? || x.infinite?)
     y = log(sqrt(x * x + 1, prec) + x, prec)
     y.round(prec)
@@ -132,6 +161,8 @@ module BigMath
   def acosh(x, prec)
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
+
+    x = x.to_d(prec) unless x.class == BigDecimal
 
     return x if x.nan?
     if x >= 1
@@ -144,6 +175,8 @@ module BigMath
   def atanh(x, prec)
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
+
+    x = x.to_d(prec) unless x.class == BigDecimal
 
     if x >= -1 && x <= 1
       half = Rational(1, 2).to_d(prec)
@@ -159,6 +192,8 @@ module BigMath
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
 
+    x = x.to_d(prec) unless x.class == BigDecimal
+
     return BigDecimal::NAN if x.nan? || y.nan?
     return BigDecimal::INFINITE if x.infinite? || y.infinite?
     sqrt(x * x + y * y, prec).round(prec)
@@ -168,39 +203,40 @@ module BigMath
     raise ArgumentError, "precision must be an Integer" unless prec.class == Integer
     raise ArgumentError, "Zero or negative precision for #{__method__}" if prec <= 0
 
-    # Positive Argument Routine
+    x = x.to_d(prec) unless x.class == BigDecimal
+
+    return BigDecimal('0', prec) if x == 1 or x == 2
+
+    # Positive Argument Routine  (Reference: Haruhiko Okumura: C-gengo ni yoru hyoujun algorithm jiten)
     positive = lambda do
       log_2pi = log(PI(prec) * 2, prec)  # $\log 2\pi$ 
       n = 8
-
-      b0 = 1r
-      b1 = -1/2r
-      b2 = 1/6r
-      b4 = -1/30r
-      b6 = 1/42r
-      b8 = -1/30r
-      b10 = 5/66r
-      b12 = -691/2730r
-      b14 = 7/6r
-      b16 = -3617/510r
-
+      coef = [
+           1/12r,     # B_2  / ( 2 *  1) =     1/12
+          -1/360r,    # B_4  / ( 4 *  3) =    -1/360
+           1/1260r,   # B_6  / ( 6 *  5) =     1/1260
+          -1/1680r,   # B_8  / ( 8 *  7) =    -1/1680
+           1/1188r,   # B_10 / (10 *  9) =     1/1188
+        -691/360360r, # B_12 / (12 * 11) =  -691/360360
+           1/156r,    # B_14 / (14 * 13) =     1/156
+       -3617/122400r  # B_16 / (16 * 15) = -3617/122400
+      ]
       v = 1
       while (x < n);  v *= x;  x += 1;  end
-      w = 1 / (x * x).to_f
+      w = 1 / (x * x)
 
-      y =  ((((((((b16 / (16 * 15))  * w + (b14 / (14 * 13))) * w\
-                + (b12 / (12 * 11))) * w + (b10 / (10 *  9))) * w\
-                + (b8  / ( 8 *  7))) * w + (b6  / ( 6 *  5))) * w\
-                + (b4  / ( 4 *  3))) * w + (b2  / ( 2 *  1))) / x\
+      y =  (((((((coef[7]  * w + coef[6]) * w + coef[5]) * w + coef[4]) * w\
+                + coef[3]) * w + coef[2]) * w + coef[1]) * w + coef[0]) / x\
                 + 0.5 * log_2pi - log(v, prec) - x + (x - 0.5) * log(x, prec)
       y.round(prec)
     end
     
     # Negative Argument Routine
     negative = lambda do
-      s = (1 / sin(PI(prec) * x, prec)).abs
+      s = (1 / sin(PI(prec) * x, prec).round(prec)).abs
+
       return BigDecimal::INFINITY if s.infinite? or s.exponent > prec
-      
+
       x = 1 - x
       y = log(s, prec) - positive.call + log(PI(prec), prec)
       y.round(prec)
@@ -208,15 +244,13 @@ module BigMath
 
     case x
     when BigDecimal::NAN
-      x
+      BigDecimal::NAN
     when BigDecimal::INFINITY
       x.negative? ? BigDecimal::NAN : BigDecimal::INFINITY
     when BigDecimal('0')
       BigDecimal::INFINITY
-    when BigDecimal # Finite
+    else # BigDecimal # Finite
       x.negative? ? negative.call : positive.call
-    else
-      raise TypeError, "not a BigDecimal"
     end
   end
 
