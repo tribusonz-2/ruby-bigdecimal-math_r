ecd42f9077e591e1882bf1d59fb92b63147e48a4
Hironobu Inatsuka <168434667+tribusonz-2@users.noreply.github.com>
2025-04-22 08:41:05 +0900

    Delete ext/bigdecimal/math_r/csech directory

--- ext/bigdecimal/math_r/csech/eulerf.h
+++
@@ -1,9 +0,0 @@
-VALUE
-csech_eulerf(VALUE z, VALUE prec)
-{
-	const ID cos = rb_intern("cos");
-	VALUE w;
-	z = rb_num_canonicalize(z, prec, ARG_COMPLEX, ARG_RAWVALUE);
-	w = trig_f_euler(cos, z, prec);
-	return rb_num_canonicalize(w, prec, ARG_COMPLEX, ARG_RECIPROCAL);
-}
