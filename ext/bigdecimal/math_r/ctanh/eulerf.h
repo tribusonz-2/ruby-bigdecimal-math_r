f0adfb832d7837fc672586f81706ac410518fb51
Hironobu Inatsuka <168434667+tribusonz-2@users.noreply.github.com>
2025-04-22 08:41:44 +0900

    Delete ext/bigdecimal/math_r/ctanh directory

--- ext/bigdecimal/math_r/ctanh/eulerf.h
+++
@@ -1,7 +0,0 @@
-VALUE
-ctanh_eulerf(VALUE z, VALUE prec)
-{
-	const ID tan = rb_intern("tan");
-	z = rb_num_canonicalize(z, prec, ARG_COMPLEX, ARG_RAWVALUE);
-	return rb_ImaginaryZ(trig_f_euler(tan, z, prec), SIGN_PLUS);
-}
