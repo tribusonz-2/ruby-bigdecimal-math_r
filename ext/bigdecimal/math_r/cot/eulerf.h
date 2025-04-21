76d147710aa72fe74fc3dd46c6e7edef5d8a36e9
Hironobu Inatsuka <168434667+tribusonz-2@users.noreply.github.com>
2025-04-22 08:40:17 +0900

    Delete ext/bigdecimal/math_r/cot directory

--- ext/bigdecimal/math_r/cot/eulerf.h
+++
@@ -1,9 +0,0 @@
-VALUE
-cot_eulerf(VALUE z, VALUE prec)
-{
-	const ID tan = rb_intern("tan");
-	VALUE w;
-	z = rb_num_canonicalize(z, prec, ARG_REAL, ARG_RAWVALUE);
-	w = trig_f_euler(tan, rb_ImaginaryZ(z, SIGN_PLUS), prec);
-	return rb_num_canonicalize(w, prec, ARG_REAL, ARG_RECIPROCAL);
-}
