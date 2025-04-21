f36bb2e6403945e0014b840df03917f1fba84014
Hironobu Inatsuka <168434667+tribusonz-2@users.noreply.github.com>
2025-04-22 08:44:50 +0900

    Delete ext/bigdecimal/math_r/tan directory

--- ext/bigdecimal/math_r/tan/eulerf.h
+++
@@ -1,8 +0,0 @@
-VALUE
-tan_eulerf(VALUE z, VALUE prec)
-{
-	const ID tan = rb_intern("tan");
-	z = rb_num_canonicalize(z, prec, ARG_REAL, ARG_RAWVALUE);
-	z = trig_f_euler(tan, rb_ImaginaryZ(z, SIGN_PLUS), prec);
-	return rb_num_real(z);
-}
