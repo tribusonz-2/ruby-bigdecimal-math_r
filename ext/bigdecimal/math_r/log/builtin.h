45b5a20f6a23cdf6b41d110b1c0f7eaa81b203eb
Hironobu Inatsuka <168434667+tribusonz-2@users.noreply.github.com>
2025-04-22 08:42:55 +0900

    Delete ext/bigdecimal/math_r/log directory

--- ext/bigdecimal/math_r/log/builtin.h
+++
@@ -1,9 +0,0 @@
-VALUE
-log_builtin(VALUE x, VALUE prec)
-{
-	const ID log = rb_intern("log");
-	if (rb_num_zero_p(x))
-		return BIG_MINUS_INF;
-	return rb_funcall(rb_mBigMath, log, 2, x, prec);
-}
-
