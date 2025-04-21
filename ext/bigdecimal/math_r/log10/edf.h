6c344210bedbc5ef74743b8cf22f8e10e48b5573
Hironobu Inatsuka <168434667+tribusonz-2@users.noreply.github.com>
2025-04-22 08:43:03 +0900

    Delete ext/bigdecimal/math_r/log10 directory

--- ext/bigdecimal/math_r/log10/edf.h
+++
@@ -1,9 +0,0 @@
-VALUE
-log10_edf(VALUE x, VALUE prec)
-{
-	VALUE fra = Qundef, exp = Qundef, y = Qundef;
-	fra = rcm10_edf(x, &exp);
-	fra = logxt_edf(fra, INT2FIX(10), prec);
-	y = rb_funcall1(exp, '+', fra);
-	return rb_num_round(y, prec);
-}
