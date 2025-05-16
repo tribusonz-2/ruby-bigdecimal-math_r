34d8c9f7143dc2a8d7bc6200421222f0cd1b2e0e
Hironobu Inatsuka <168434667+tribusonz-2@users.noreply.github.com>
2025-05-16 19:05:52 +0900

    Bugfixes

--- ext/bigdecimal/math_r/internal/algo/bigmath/asec/branch.h
+++ ext/bigdecimal/math_r/internal/algo/bigmath/asec/branch.h
@@ -12,7 +12,7 @@ asec_branch(VALUE x, VALUE prec, bigmath_func1 asec_cb)
 		y = BIG_NAN;
 	else if (rb_num_infinite_p(x) != 0)
 		y = rb_funcall(rb_bigmath_const_pi(prec), div, 2, INT2FIX(2), prec);
-	else if (RTEST(rb_num_coerce_bin(x, INT2FIX(1), leq)) ||
+	else if (RTEST(rb_num_coerce_bin(x, INT2FIX(-1), leq)) ||
 	         RTEST(rb_num_coerce_bin(x, INT2FIX(1), geq)))
 		y = asec_cb(x, prec);
 	else
