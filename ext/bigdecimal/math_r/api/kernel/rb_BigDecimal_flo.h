794228972c14a61f69986319d6d5877f2faf2b2d
Hironobu Inatsuka <168434667+tribusonz-2@users.noreply.github.com>
2025-06-15 21:23:09 +0900

    Fix NaN handling
    If `x` is `Float::NAN`, an error occurred because it cannot be converted to `BigDecimal::NAN`.

--- ext/bigdecimal/math_r/api/kernel/rb_BigDecimal_flo.h
+++ ext/bigdecimal/math_r/api/kernel/rb_BigDecimal_flo.h
@@ -3,7 +3,13 @@ rb_BigDecimal_flo(VALUE x)
 {
 	const ID BigDecimal = rb_intern("BigDecimal");
 	const ID DIG = rb_intern("DIG");
-	VALUE FLOAT_DIG = rb_const_get_at(rb_cFloat, DIG);
-	VALUE y = rb_funcall(rb_cObject, BigDecimal, 2, x, FLOAT_DIG);
+	VALUE FLOAT_DIG, y;
+	if (rb_num_nan_p(x))
+		y = BIG_NAN;
+	else
+	{
+		FLOAT_DIG = rb_const_get_at(rb_cFloat, DIG);
+		y = rb_funcall(rb_cObject, BigDecimal, 2, x, FLOAT_DIG);
+	}
 	return y;
 }
