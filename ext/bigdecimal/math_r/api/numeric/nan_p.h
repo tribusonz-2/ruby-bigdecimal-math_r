2879183026eca1574405bd087099f85f7099a682
Hironobu Inatsuka <168434667+tribusonz-2@users.noreply.github.com>
2025-06-15 21:35:20 +0900

    Support for NaN in Complex

--- ext/bigdecimal/math_r/api/numeric/nan_p.h
+++ ext/bigdecimal/math_r/api/numeric/nan_p.h
@@ -1,5 +1,8 @@
 bool
 rb_num_nan_p(VALUE z)
 {
-	return rb_num_notequal_p(z, z);
+	if (rb_num_real_p(z))
+		return rb_num_notequal_p(z, z);
+	else
+		return !rb_num_finite_p(z) && !rb_num_infinite_p(z);
 }
