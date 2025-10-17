b203517b49d5014b6e9dc98c640c45815d327ea9
tribusonz-2 <xwisepp62543@gmail.com>
2025-10-17 19:40:36 +0900

    Bugfix: invalid return value when argument 0

--- ext/bigdecimal/math_r/internal/algo/bigmath/asec/edom.h
+++ ext/bigdecimal/math_r/internal/algo/bigmath/asec/edom.h
@@ -8,9 +8,13 @@ asec_edom(VALUE x, VALUE prec)
 
 	x = rb_num_canonicalize(x, n, ARG_REAL, ARG_RECIPROCAL);
 	t = rb_funcall1(BIG_ONE, '-', rb_funcall1(x, '*', x));
+	// Bug-fix: Below, if the argument is -0.0, the calculation is Infinity-Infinity, resulting in NaN.
 	t = rb_funcall1(rb_bigmath_sqrt(rb_num_uminus(t), n), '+', x);
 	arg = rb_num_negative_p(x) ? rb_bigmath_const_pi(n) : BIG_ZERO;
 	imag = rb_bigmath_log(rb_num_abs(t), n);
+	// Bug-Fix: Set to -Infinity if NaN
+	if (rb_num_nan_p(imag))
+		imag = BIG_MINUS_INF;
 	y = rb_Complex(arg, imag);
 
 	return rb_num_round(y, prec);
