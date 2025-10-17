74a211e60feb3949de297052bd15838070d99602
tribusonz-2 <xwisepp62543@gmail.com>
2025-10-18 08:31:18 +0900

    Replace the routine for complex argument

--- ext/bigdecimal/math_r/internal/algo/bigmath/casech/branch.h
+++ ext/bigdecimal/math_r/internal/algo/bigmath/casech/branch.h
@@ -10,20 +10,13 @@ casech_branch(VALUE z, VALUE prec, bigmath_func1 casech_cb)
 	else if (z_re_inf || z_im_inf)
 	{
 		const ID div = rb_intern("div");
-		if (z_re_inf != 0 && z_im_inf == 0)
+		if ((z_re_inf != 0 && z_im_inf == 0) ||
+		    (z_re_inf == 0 && z_im_inf != 0))
 		{
 			VALUE imag = rb_funcall(rb_bigmath_const_pi(prec), div, 
 				2, INT2FIX(2), prec);
 			w = rb_Complex(BIG_ZERO, imag);
 		}
-		else if (z_re_inf == 0 && z_im_inf != 0)
-		{
-			VALUE imag = rb_funcall(rb_bigmath_const_pi(prec), div, 
-				2, INT2FIX(-2 * z_im_inf), prec);
-			w = rb_Complex(BIG_ZERO, imag);
-		}
-		else if (z_re_inf == -1 && z_im_inf == 1)
-			w = rb_Complex(BIG_NAN, BIG_NAN);
 		else
 			w = rb_Complex(BIG_ZERO, BIG_ZERO);
 	}
@@ -35,4 +28,3 @@ casech_branch(VALUE z, VALUE prec, bigmath_func1 casech_cb)
 
 	return w;
 }
-
