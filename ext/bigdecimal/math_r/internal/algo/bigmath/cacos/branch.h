17b3dd8d6fb0648291e65bb7b3e361502a435925
tribusonz-2 <xwisepp62543@gmail.com>
2025-10-17 12:59:52 +0900

    Replace the routine for complex argument

--- ext/bigdecimal/math_r/internal/algo/bigmath/cacos/branch.h
+++ ext/bigdecimal/math_r/internal/algo/bigmath/cacos/branch.h
@@ -1,6 +1,8 @@
 VALUE
 cacos_branch(VALUE z, VALUE prec, bigmath_func1 cacos_cb)
 {
+	const ID mult = rb_intern("mult");
+	const ID div = rb_intern("div");
 	int z_re_inf = rb_num_infinite_p(rb_num_real(z));
 	int z_im_inf = rb_num_infinite_p(rb_num_imag(z));
 
@@ -14,20 +16,26 @@ cacos_branch(VALUE z, VALUE prec, bigmath_func1 cacos_cb)
 				(rb_num_zero_p(rb_num_imag(z)) &&
 			         z_re_inf == 1) ?
 				BIG_ZERO : rb_bigmath_const_pi(prec);
-			VALUE imag = z_re_inf == 1 ? BIG_MINUS_INF : BIG_INF;
+			VALUE imag = BIG_MINUS_INF;
 			return rb_Complex(real, imag);
 		}
 		else if (z_re_inf == 0 && z_im_inf != 0)
 		{
-			return rb_Complex(BIG_ZERO, rb_num_imag(z));
-		}
-		else if (z_re_inf == -1 && z_im_inf == 1)
-		{
-			return rb_Complex(BIG_NAN, BIG_NAN);
+			VALUE real = rb_bigmath_const_pi(prec);
+			VALUE imag = z_im_inf == 1 ? BIG_MINUS_INF : BIG_INF;
+			if (!rb_num_negative_p(rb_num_real(z)))
+				real = rb_funcall(real, div, 2, INT2FIX(2), prec);
+			return rb_Complex(real, imag);
 		}
 		else
 		{
-			return rb_Complex(BIG_ZERO, BIG_ZERO);
+			VALUE real = rb_bigmath_const_pi(prec);
+			VALUE imag = z_im_inf == -1 ? BIG_INF : BIG_MINUS_INF;
+			real = z_re_inf == -1 ?
+				rb_funcall(real, mult, 2, 
+					rb_rational_new(INT2FIX(3), INT2FIX(4)), prec) :
+				rb_funcall(real, div, 2, INT2FIX(4), prec);
+			return rb_Complex(real, imag);
 		}
 	}
 	else
