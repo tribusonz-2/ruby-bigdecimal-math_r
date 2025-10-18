af2b5b9464fb070d72f2726637caf4b6d5730138
tribusonz-2 <xwisepp62543@gmail.com>
2025-10-18 10:02:58 +0900

    Replace the routine for complex argument

--- ext/bigdecimal/math_r/internal/algo/bigmath/casin/branch.h
+++ ext/bigdecimal/math_r/internal/algo/bigmath/casin/branch.h
@@ -9,23 +9,38 @@ casin_branch(VALUE z, VALUE prec, bigmath_func1 casin_cb)
 	else if (z_re_inf || z_im_inf)
 	{
 		const ID div = rb_intern("div");
+		const ID mult = rb_intern("mult");
+		VALUE real, imag;
 		if (z_re_inf != 0 && z_im_inf == 0)
 		{
-			VALUE real = rb_num_zero_p(rb_num_imag(z)) ?
+			real = 
 				rb_funcall(rb_bigmath_const_pi(prec), div, 2, 
-					INT2FIX(z_re_inf * 2), prec) :
-				BIG_ZERO;
-			VALUE imag = z_re_inf == 1 ? BIG_MINUS_INF : BIG_INF;
+					INT2FIX(z_re_inf * 2), prec);
+			imag = z_re_inf == 1 ? BIG_MINUS_INF : BIG_INF;
 			return rb_Complex(real, imag);
 		}
 		else if (z_re_inf == 0 && z_im_inf != 0)
 		{
-			return rb_Complex(BIG_ZERO, rb_num_imag(z));
+			real = rb_bigmath_const_pi(prec);
+			imag = BIG_MINUS_INF;
+			if (z_im_inf == -1)
+				real = rb_funcall(real, div, 2, INT2FIX(2), prec);
+			return rb_Complex(real, imag);
 		}
-		else if (z_re_inf == -1 && z_im_inf == 1)
-			return rb_Complex(BIG_NAN, BIG_NAN);
 		else
-			return rb_Complex(BIG_ZERO, BIG_ZERO);
+		{
+			VALUE rat = Qundef;
+			real = rb_bigmath_const_pi(prec);
+			imag = BIG_MINUS_INF;
+			if (z_re_inf == 1 && z_im_inf == 1)
+				rat = rb_rational_new(INT2FIX(5), INT2FIX(4));
+			else if (z_re_inf == 1 && z_im_inf == -1)
+				rat = rb_rational_new(INT2FIX(3), INT2FIX(4));
+			else
+				rat = rb_rational_new(INT2FIX(1), INT2FIX(4));
+			real = rb_funcall(real, mult, 2, rat, prec);
+			return rb_Complex(real, imag);
+		}
 	}
 	else
 	{
